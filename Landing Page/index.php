6c6be53f73be4d270d4d6d850cf95f7341ce1cee
QuantumDial <31623114+AkshatChandel@users.noreply.github.com>
2022-01-26 16:20:30 +0530

    Landing Page

---
+++ Landing Page/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+
+    <link href="normalize.css" rel="stylesheet">
+
+    <link href="stylesheet.css" rel="stylesheet">
+
+</head>
+
+<body>
+    
+    <div class="wrapper">
+        <div class="side left">
+            <div class="image training"></div>
+            <div class="caption">
+                <h1>Training</h1>
+                <a class="button" href="#">Training</a>
+            </div>
+        </div>
+        <div class="side right">
+            <div class="image placement"></div>
+            <div class="caption">
+                <h1>Placement</h1>
+                <a class="button" href="#">Placement</a>
+            </div>
+        </div> 
+
+    </div>
+</body>
+</html>
