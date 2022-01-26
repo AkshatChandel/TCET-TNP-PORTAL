a8f3bb0b3d09aecffb8c9d4b11e44440aa46cdc1
yash0201 <56736232+yash0201@users.noreply.github.com>
2022-01-26 16:54:58 +0530

    Landing Page moved to Student folder

--- Student/index.php
+++ Student/index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
 
-    <link href="normalize.css" rel="stylesheet">
+    <link href="../CSS/normalize.css" rel="stylesheet">
 
-    <link href="stylesheet.css" rel="stylesheet">
+    <link href="../CSS/style.css" rel="stylesheet">
 
 </head>
 
 <body>
-    
+
     <div class="wrapper">
         <div class="side left">
             <div class="image training"></div>
@@ -28,8 +29,9 @@
                 <h1>Placement</h1>
                 <a class="button" href="#">Placement</a>
             </div>
-        </div> 
+        </div>
 
     </div>
 </body>
+
 </html>
